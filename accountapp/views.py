797e345a53b022677dfcdf7b0289bd5c9007b500
assofiejakobsson <>
2023-05-23 20:58:45 +0000

    Created registrations view and path in accountapp.

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from todo.models import Todo
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('accountapp:game')
+
+    else:
+        form = UserCreationForm()
+    return render(request, 'accountapp/register.html', {'form': form})
