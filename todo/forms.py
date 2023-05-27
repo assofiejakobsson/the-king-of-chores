c56ddbd33aae26c2bbf2f6615f7b85cedd8d5d78
assofiejakobsson <>
2023-05-27 15:44:34 +0000

    Added a new form in forms.py for collaborators.

--- todo/forms.py
+++ todo/forms.py
@@ -1,8 +1,21 @@
 from django import forms
-from .models import Todo
+from game.models import Todo, Game
+from django.contrib.auth.models import User
 
 
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
         fields = ('title', 'description', 'completed', 'points')
+
+
+class TodoCollaboratorForm(forms.ModelForm):
+    collaborators = forms.ModelMultipleChoiceField(
+        queryset=User.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        requierd=False
+    )
+
+    class Meta:
+        model = Game
+        fields = ['collaborators']
