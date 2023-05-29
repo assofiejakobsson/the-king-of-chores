1db0af1b42f61c6cfe68cb42f6bac848a46ddffa
assofiejakobsson <s89j@live.se>
2023-05-29 11:23:03 +0000

    Added a new form GuestForm

--- game/forms.py
+++ game/forms.py
@@ -2,6 +2,12 @@ from django import forms
 from .models import Todo, Game
 
 
+class GuestForm(forms.ModelForm):
+   class Meta:
+       model = Guest
+       fields = ['name', 'email']
+
+
 class GameForm(forms.ModelForm):
     todo = forms.ModelChoiceField(queryset=Todo.objects.all())
 
