732acbeb284caa43c09175c7865e99704cffd08b
assofiejakobsson <s89j@live.se>
2023-05-31 23:08:18 +0000

    Removed a incorect path in urls.py. And updated form action in the todo_list.html

--- todo/urls.py
+++ todo/urls.py
@@ -7,7 +7,6 @@ app_name = 'todo'
 urlpatterns = [
     path('', views.todo_list, name='todo_list'),
     path('create/', views.todo_create, name='todo_create'),
-    path('create_todo/', views.create_todo, name='create_todo'),
     path('update/<int:pk>/', views.todo_update, name='todo_update'),
     path('delete/<int:pk>/', views.todo_delete, name='todo_delete'),
     path('guest_complete/<int:guest_id>/', views.todo_guest_complete, name='todo_guest_complete'),
