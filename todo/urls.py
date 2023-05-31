f0af3bab4882b4306fe65697c43989b4fcad67bd
assofiejakobsson <s89j@live.se>
2023-05-31 23:45:09 +0000

    Updated the urls.py in the todo app whit all urls in the projrct.

--- todo/urls.py
+++ todo/urls.py
@@ -5,14 +5,15 @@ from . import views
 app_name = 'todo'
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.todo_list, name='todo_list'),
     path('create/', views.todo_create, name='todo_create'),
     path('create_todo/', views.create_todo, name='create_todo'),
     path('update/<int:pk>/', views.todo_update, name='todo_update'),
     path('delete/<int:pk>/', views.todo_delete, name='todo_delete'),
     path('guest_complete/<int:guest_id>/', views.todo_guest_complete, name='todo_guest_complete'),
+    path('accounts/', include('accountapp.urls')),
 ]
-
 
 """ urlpatterns = [
     path('', views.todo_list, name='todo_list'),
