e2b1d376e034251cb11cebd0119c51dfcef96875
assofiejakobsson <s89j@live.se>
2023-06-01 14:06:53 +0000

    Updated the import in the urls.py in the project TheKingOfChores

--- TheKingOfChores/urls.py
+++ TheKingOfChores/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .todo import views
-
+from todo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
