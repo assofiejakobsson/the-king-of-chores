e4a3b5399c3d744648b1fd2598081734cbc0fe62
assofiejakobsson <s89j@live.se>
2023-06-01 13:12:58 +0000

    Updated the urls.py in the todo app and the project TheKingOfChores.

--- todo/urls.py
+++ todo/urls.py
@@ -1,19 +1,17 @@
-from django.contrib import admin
-from django.urls import include, path
-from . views import todo_list, todo_create, todo_delete, todo_update, todo_guest_complete
-from . import views
+from django.urls import path
+from .views import todo_list, todo_create, todo_delete, todo_update, todo_guest_complete, create_todo
 
 app_name = 'todo'
 
 urlpatterns = [
-    
-    path('', views.todo_list, name='todo_list'),
-    path('create/', views.todo_create, name='todo_create'),
-    path('update/<int:pk>/', views.todo_update, name='todo_update'),
-    path('delete/<int:pk>/', views.todo_delete, name='todo_delete'),
-    path('guest_complete/<int:guest_id>/', views.todo_guest_complete, name='todo_guest_complete'),
-    
+    path('', todo_list, name='todo_list'),
+    path('create/', todo_create, name='todo_create'),
+    path('update/<int:pk>/', todo_update, name='todo_update'),
+    path('delete/<int:pk>/', todo_delete, name='todo_delete'),
+    path('guest_complete/<int:guest_id>/', todo_guest_complete, name='todo_guest_complete'),
+    path('create_todo/', create_todo, name='create_todo'),
 ]
+
 
 """ urlpatterns = [
     path('', views.todo_list, name='todo_list'),
