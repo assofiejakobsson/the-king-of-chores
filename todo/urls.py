dd3eb69a79f983997ea94cf49bc4f47537074205
assofiejakobsson <s89j@live.se>
2023-06-02 14:00:34 +0000

    Updated the imports for view in the todo app urls.py

--- todo/urls.py
+++ todo/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from . views import todo_list, create_todo, todo_delete, todo_update, todo_guest_complete
+from .views import todo_list, create_todo, todo_delete, todo_update, todo_guest_complete
 
 app_name = 'todo'
 
 urlpatterns = [
-    path('', views.todo_list, name='todo_list'),
-    path('create/', views.create_todo, name='create_todo'),
-    path('update/<int:pk>/', views.todo_update, name='todo_update'),
-    path('delete/<int:pk>/', views.todo_delete, name='todo_delete'),
-    path('guest_complete/<int:guest_id>/', views.todo_guest_complete, name='todo_guest_complete'),
+    path('', todo_list, name='todo_list'),
+    path('create/', create_todo, name='create_todo'),
+    path('update/<int:pk>/', todo_update, name='todo_update'),
+    path('delete/<int:pk>/', todo_delete, name='todo_delete'),
+    path('guest_complete/<int:guest_id>/', todo_guest_complete, name='todo_guest_complete'),
 ]
 
 """ urlpatterns = [
