1708dbe68cdddbe350239ad462c34d0b4099b454
assofiejakobsson <s89j@live.se>
2023-06-10 10:45:09 +0000

    Added user, created_at and updated_at to the Todo model.

--- todo/models.py
+++ todo/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)  
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
-    completed_by = models.CharField(max_length=100, null=True, blank=True)
+    completed_by = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
-def __str__(self):
+    def __str__(self):
         return self.title
 
+        
 
 """ class Todo(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
