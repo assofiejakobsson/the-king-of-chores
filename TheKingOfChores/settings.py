d404c1ea3bdcecc470c9b4694f4e3b0df4cbd9c7
assofiejakobsson <s89j@live.se>
2023-06-14 22:36:38 +0000

    Commented out code that caused problems in settings.py. And fixed the link to css in base.html.

--- TheKingOfChores/settings.py
+++ TheKingOfChores/settings.py
@@ -148,13 +148,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+""" STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles'),
 ]
-
+ """
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
