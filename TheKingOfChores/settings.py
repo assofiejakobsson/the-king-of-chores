9f5db702fa3513bedc6dc37a5f07df9353d8011e
assofiejakobsson <s89j@live.se>
2023-06-19 04:48:43 +0000

    Change the DEBUG to False

--- TheKingOfChores/settings.py
+++ TheKingOfChores/settings.py
@@ -32,7 +32,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'the-king-of-chores.herokuapp.com',
